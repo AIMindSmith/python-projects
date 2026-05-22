4f066d2c39cd4653945ecd4bd237c11e62596828
NeuroByteForge <atulbambatkar12345@gmail.com>
2026-05-22 17:06:27 +0530

    adding newline

--- choose_your_advanture.py
+++ choose_your_advanture.py
@@ -1,7 +1,7 @@
-name = input("What is your name? ")
+name = input("What is your name? :\n  ")
 print("Hello " + name + ", welcome to the adventure game!")
 
-answer = input("You are in a dark room. There is a door to your left and right. Which one do you take? (left/right) ").lower()
+answer = input("You are in a dark room. There is a door to your left and right. Which one do you take? (left/right) :\n ").lower()
 if answer =="left":
     print("You enter a room full of beasts. Game Over.")
 elif answer == "right":
@@ -11,14 +11,14 @@ else:
 
 
 
-answer = input("You see a river. Do you want to swim across or build a raft? (swim/raft) ").lower()
+answer = input("You see a river. Do you want to swim across or build a raft? (swim/raft) :\n ").lower()
 if answer == "swim":
     print("You were eaten by a crocodile. Game Over.")
 elif answer == "raft":
     print("You safely crossed the river and found a village. You Win!")
 else:
     print("You choose a path that doesn't exist. Game Over.")
-answer = input("You enter a village. There is a merchant who offers you a sword. Do you want to buy it? (yes/no) ").lower()
+answer = input("You enter a village. There is a merchant who offers you a sword. Do you want to buy it? (yes/no) :\n ").lower()
 if answer == "yes":
     print("You bought the sword and used it to defeat the dragon. You Win!")
 elif answer == "no":
